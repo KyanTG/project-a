004e6e0f3e8c8de9190e7287d0ded41d520ff983
KyanTG <kyan.gijtenbeek@hva.nl>
2025-11-17 11:14:20 +0100

    working on mini project

---
+++ project/src/app/app.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home';
+import { HeaderComponent } from './components/header/header';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  template: `
+    <app-header />
+    <app-home />
+  `,
+})
+export class App {
+  title = 'first-app';
+}
